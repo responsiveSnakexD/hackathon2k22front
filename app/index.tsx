389a09decfa7847e8273919f9902a9ad17184ad9
wleklinskimateusz <wleklinskimateusz05@gmail.com>
2022-11-19 16:37:33 +0100

    add icons, create modal

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import {InfoComponent} from '../src/components/InfoComponent';
 
 const App: React.FC = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!!!</Text>
+      <InfoComponent />
       <StatusBar style="auto" />
     </View>
   );
