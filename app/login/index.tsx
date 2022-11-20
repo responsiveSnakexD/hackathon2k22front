afe50ba27a411de14481634d25ab962c2c3363fc
Inevera11 <zuzannabozek2002@gmail.com>
2022-11-20 09:32:17 +0100

    data

--- app/login/index.tsx
+++ app/login/index.tsx
@@ -37,7 +37,7 @@ const Login: React.FC<PageProps> = ({navigation}) => {
       navigation.navigate('index');
     } else {
       auth.send('LOGOUT');
-      setError('złe dane!');
+      setError('incorrect data');
     }
   };
 
